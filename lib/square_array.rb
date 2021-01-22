a36b0a5bed7df2b087d79242cdf2e16f0047f4e4
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-22 22:21:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,21 @@
+ # require 'pry'
+
 def square_array(array)
-  # your code here
+i = 0
+# new_array = [1,2,3]
+ while i< array.length do
+  puts  array[i] ** 2
+   i +=1
+ end
 end
+
+
+
+
+# counter = 0
+# array = [1,2,3,4,5]
+#
+# while array[counter] do
+#   puts array[counter]
+#   counter += 1
+# end
