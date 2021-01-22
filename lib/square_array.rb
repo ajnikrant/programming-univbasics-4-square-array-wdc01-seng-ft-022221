07636fdaca2037a29e31dd282a62f98ef883203b
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-22 22:41:16 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+ # require 'pry'
+
 def square_array(array)
-  # your code here
+i = 0
+# newarray = [1,2,3]
+ while i < array.length do
+  array[i]= (array[i] ** 2)
+   i +=1
+  # return array
+ end
+ return array
 end
